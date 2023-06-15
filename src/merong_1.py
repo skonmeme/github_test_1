abd5e7987be3e779a2adeab5744dff992cef90d7
Sung Gon Yi <skonmeme@gmail.com>
2023-06-15 12:40:13 +0900

    3

--- src/merong_1.py
+++ src/merong_1.py
@@ -1,5 +1,7 @@
+from datetime import datetime, timezone, timedelta
 import os
 
 if __name__ == '__main__':
     print('Hello, World!')
+    print(datetime.now(timezone.utc) + timedelta(hours=9))
     print(os.getcwd())
